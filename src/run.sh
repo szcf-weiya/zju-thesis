2808c0af725c0ce1c9df3c8c264b6a49c4e01373
szcf-weiya <2215235182@qq.com>
2018-04-11 18:00:36 +0800

    overall framework

--- src/run.sh
+++ src/run.sh
@@ -3,4 +3,5 @@ latex zju-thesis.ins
 cp zju-thesis.cls ../demo
 make clean -C ../demo
 make -C ../demo
-rm zju-thesis.cls zju-thesis.sty
+make clean -C ../demo
+rm -f zju-thesis.cls zju-thesis.sty *.log *.aux *.synctex.gz *.toc *.bbl *.bcf *.blg *.out *.xml *.idx *.dvi *.glo *.hd
