bef0645316bbbd852820b0f602fd5450089fac9a
szcf-weiya <2215235182@qq.com>
2018-04-15 11:39:04 +0800

    rm local scripts

--- src/run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-latex zju-thesis.ins
-cp zju-thesis.cls ../demo
-make clean -C ../demo
-make -C ../demo
-make clean -C ../demo
-rm -f zju-thesis.cls zju-thesis.sty *.log *.aux *.synctex.gz *.toc *.bbl *.bcf *.blg *.out *.xml *.idx *.dvi *.glo *.hd
