8fee4d4371f2810c6f3a79564cda9df38f19013c
szcf-weiya <2215235182@qq.com>
2018-05-13 23:14:09 +0800

    release v1.0.6

--- release.sh
+++ release.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 rm -f zju-thesis_v*.*.*.tar.gz
 rm -f zju-proposal_v*.*.*.tar.gz
-tar -czf zju-thesis_v1.0.5.tar.gz demo/ assets/ src/
+tar -czf zju-thesis_v1.0.6.tar.gz demo/ assets/ src/
 #tar -czf zju-proposal_v1.0.0.tar.gz proposal/
